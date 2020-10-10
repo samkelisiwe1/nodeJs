5e14f30f3b6ecb42b6845b460af081dad38a4841
sam <sam@thedigitalacademy.co.za>
2020-10-11 00:04:21 +0200

    cors added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+var cors = require('cors')
 const bodyParser = require('body-parser');
 
-
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));   
 app.use(bodyParser.json({}));
+
 
 //user route
  const userRoute  = require('./routes/user');
