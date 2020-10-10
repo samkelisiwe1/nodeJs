df84b9e7c46698f3089bf8cf652f107c9a549a9a
sam <sam@thedigitaladacademy.co.za>
2020-10-10 16:27:44 +0200

    log in

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router()
 const con = require('../connection');
 
+
+//register new user
 router.post('/register', (req, res) => {
 
     const { userName, lastName, Email, idNumber, Userpassword, userType } = req.body;
 
  
-
     con.query('INSERT INTO user SET ?', {userName:userName,lastName:lastName,Email:Email,idNumber:idNumber,Userpassword:Userpassword,userType:userType }, (error, results) => {
 
         if (error) {
@@ -24,6 +25,42 @@ router.post('/register', (req, res) => {
             });
         }
     })
+});
+
+
+//logging  in
+router.post('/login',(req,res) => {
+    
+    try {
+        const  { Email , Userpassword } = req.body;
+
+        if (!Email || !Userpassword) {
+            res.send('please provide an email and password')
+        }
+
+        let sql = 'SELECT `UserID`, `userName`,`lastName` `Email`, `idNumber` `Userpassword` `userType` FROM `user` WHERE Email = ?'
+
+        con.query(sql, [Email], (error, results) => {
+
+            console.log(results)
+
+            // if (Userpassword == res) {
+            //     res.send(results)
+            //     console.log(Userpassword, results.Userpassword)
+
+
+            // } else {
+
+            //     res.send('Invalid credentials')
+            // }
+
+        })
+
+    } catch (error) {
+
+        console.log(error)
+    }
 })
+
 
 module.exports = router;
